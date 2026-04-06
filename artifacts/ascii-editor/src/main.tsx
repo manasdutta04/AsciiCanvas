5d951dfe8755a3eecefeb7dcd3169b4392d4c9a5
Manas Dutta <122201926+manasdutta04@users.noreply.github.com>
2026-04-06 10:47:24 +0530

    update the route

--- artifacts/ascii-editor/src/main.tsx
+++ artifacts/ascii-editor/src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { Router, Route } from "wouter";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router>
+    <Route path="/draw" component={App} />
+    <Route path="/" component={App} />
+    <Route path="/:path*" component={App} />
+  </Router>,
+);
